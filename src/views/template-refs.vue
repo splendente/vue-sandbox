b3c768cf6be837a6a5d347057fb6c4d1982b9d16
Hikaru Kobayashi <hikaru.kobayashi0719+devlop@gmail.com>
2024-07-07 09:39:54 +0900

    :sparkles: create watchers-view and template-refs-view

--- src/views/template-refs.vue
+++ src/views/template-refs.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const list = ref(null)
+const listWidth = computed(() => list.value?.clientWidth)
+</script>
+
 <template>
   <main>
     <h1>Template Refs</h1>
+    <ul ref="list">
+      <li v-for="(item, index) in 5" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+    <p>
+      Size of list is <span>{{ listWidth }}</span> px
+    </p>
   </main>
 </template>
--- src/views/watchers-view.vue
+++ src/views/watchers-view.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import { ref, watch, watchEffect } from 'vue'
+
+const count = ref(0)
+watch(count, () => {
+  console.log('count is called' + count.value + ' times')
+})
+count.value++
+
+const count2 = ref(0)
+watchEffect(() => {
+  console.log('count2 is called' + count2.value + ' times')
+})
+count2.value++
+</script>
+
 <template>
   <main>
     <h1>Watchers</h1>
